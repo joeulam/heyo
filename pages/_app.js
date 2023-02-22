26994ec6fa2b100b53889b456e2e0d7a2d1f3b0f
joeulam <joeulam345@gmail.com>
2023-02-22 16:57:40 -0500

    first update

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+
+import styles from '@/styles/login.css'
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Heyo</title>
+      </Head>
+      
+
+      <main>
+        <h1>Heyo</h1>
+
+        <div>
+          <h2>
+            
+          </h2>
+        </div>
+      </main>
+    </>
+  )
+}
