4ce8184314645dbaf9db830aff45198fed4a5650
joeulam <joeulam345@gmail.com>
2023-02-26 21:28:27 -0500

    Added functions to index,js for abt and contact us

---
+++ pages/About_us/index.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export default function about() {
+
+    return(
+        <>
+            <head>
+                <title>About Us</title>
+            </head>
+
+            <main>
+                <div>
+                About Us                
+                </div>
+                
+            </main>
+
+
+
+
+        </>
+    )
+}
---
+++ pages/Contact_us/index.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export default function contact() {
+
+    return(
+        <>
+            <head>
+                <title>Contact Us</title>
+            </head>
+
+            <main>
+                <div>
+                Contact Us                
+                </div>
+                
+            </main>
+
+
+
+
+        </>
+    )
+}
