6f1a06f6af70208590ab6e883bc5e3750a5179e9
joeulam <joeulam345@gmail.com>
2023-03-01 00:21:01 -0500

    ios landing page completed

--- pages/index.js
+++ pages/index.js
@@ -10,39 +10,34 @@ export default function Home() {
     <>
       <Head>
         <title>Heyo</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
       </Head>
       
 
       <main>
         <div class="navb">
-        <nav1 class="nav1 main">
+          <nav1 class="nav1 main">
 
-          <div1 class="left">
-            <h1>Heyo</h1>
-          </div1>
+            <div1 class="left">
+              <h1>Heyo</h1>
+            </div1>
 
-          <div class="right end">
-            <Link href="/Contact_us">
-            <h3>Contact Us</h3>
+            <div class="right end">
+              <Link href="/Contact_us">
+              <h3>Contact Us</h3>
 
-            </Link>
+              </Link>
 
 
-          </div>
+            </div>
 
-          <div class="right">
-          <Link href="/About_us">
+            <div class="right">
+            <Link href="/About_us">
 
-            <h3>About Us</h3>
-            </Link>
-          </div>
-
-          <div class="right">
-            <Link href="/logins">
-              <h3>Login</h3>
-            </Link>         
-          </div>
-        </nav1>
+              <h3>About Us</h3>
+              </Link>
+            </div>
+          </nav1>
         </div>
         
         <div class="body">
@@ -54,12 +49,20 @@ export default function Home() {
             <p>wbuawudwuowd</p>
           </div>
 
-          <div class="main sign">
-          <Link href="/signup">
-              <h3>Sign up</h3>
-            </Link>         
+          <div class="buttondiv">
+              <div class="sign">
+                <Link href="/signup">
+                    <h3>Sign up</h3>
+                  </Link>         
+              </div>
+              <div class="sign ends">
+                <Link href="/logins">
+                  <h3>Login</h3>
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
+          
         
 
 
