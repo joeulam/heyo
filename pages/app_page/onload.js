1acf60bb5aaa73f7bdff4b57fb2f756a7ff0decd
Joey Lam <joeulam345@gmail.com>
2023-03-13 14:13:57 -0400

    - Fixed firebase database structure - Added routing - New mood and quick write index

--- pages/app_page/onload.js
+++
@@ -1,7 +0,0 @@
-
-
-import { userdata } from '@/lib/app_pagecomms';
-function ondone(){
-    console.log("RUNNING")
-    document.getElementById("userlogged").innerHTML = "The current user: " + userdata.displayName
-}
